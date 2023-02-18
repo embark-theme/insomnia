3c724c85afadca1b9014d5acbb9237614aeef28e
Stephen Bolton <stephen@bitsonthemind.com>
2023-02-18 15:53:32 -0500

    Update light cyan color
    This was changed in the parent palette.

--- themes/embark.js
+++ themes/embark.js
@@ -1,70 +1,70 @@
 module.exports = {
-	name: 'embark',
-	displayName: 'Embark',
-	theme: {
-		background: {
-			default: '#1e1c31',
-			success: '#a1efd3',
-			notice: '#ffe6b3',
-			warning: '#f2b482',
-			danger: '#f48fb1',
-			surprise: '#d4bfff',
-			info: '#91ddff'
-		},
-		foreground: {
-			default: '#cbe3e7',
-			success: '#1e1c31',
-			notice: '#1e1c31',
-			warning: '#1e1c31',
-			danger: '#1e1c31',
-			surprise: '#1e1c31',
-			info: '#1e1c31'
-		},
-		highlight: {
-			default: '#8A889D',
-		},
-		styles: {
-			appHeader: {
-				background : {
-					default: '#100e23'
-				}
-			},
-			sidebar: {
-				background: {
-					default: '#100e23'
-				},
-				highlight: {
-					default: "#cbe3e7"
-				}
-			},
-			dialog: {
-				background: {
-					default: '#100e23'
-				}
-			},
-			tooltip: {
-				background: {
-					default: '#100e23'
-				},
-				foreground: {
-					default: '#cbe3e7'
-				}
-			},
-			paneHeader: {
-				background: {
-					success: '#a1efd3',
-					notice: '#ffe6b3',
-					warning: '#f2b482',
-					danger: '#f48fb1',
-					surprise: '#d4bfff',
-					info: '#87dfeb'
-				}
-			},
-			transparentOverlay: {
-				background: {
-					default: 'rgba(16, 14, 35, 0.6)'
-				}
-			}
-		}
-	}
+  name: 'embark',
+  displayName: 'Embark',
+  theme: {
+    background: {
+      default: '#1e1c31',
+      success: '#a1efd3',
+      notice: '#ffe6b3',
+      warning: '#f2b482',
+      danger: '#f48fb1',
+      surprise: '#d4bfff',
+      info: '#91ddff'
+    },
+    foreground: {
+      default: '#cbe3e7',
+      success: '#1e1c31',
+      notice: '#1e1c31',
+      warning: '#1e1c31',
+      danger: '#1e1c31',
+      surprise: '#1e1c31',
+      info: '#1e1c31'
+    },
+    highlight: {
+      default: '#8A889D',
+    },
+    styles: {
+      appHeader: {
+        background: {
+          default: '#100e23'
+        }
+      },
+      sidebar: {
+        background: {
+          default: '#100e23'
+        },
+        highlight: {
+          default: "#cbe3e7"
+        }
+      },
+      dialog: {
+        background: {
+          default: '#100e23'
+        }
+      },
+      tooltip: {
+        background: {
+          default: '#100e23'
+        },
+        foreground: {
+          default: '#cbe3e7'
+        }
+      },
+      paneHeader: {
+        background: {
+          success: '#a1efd3',
+          notice: '#ffe6b3',
+          warning: '#f2b482',
+          danger: '#f48fb1',
+          surprise: '#d4bfff',
+          info: '#ABF8F7'
+        }
+      },
+      transparentOverlay: {
+        background: {
+          default: 'rgba(16, 14, 35, 0.6)'
+        }
+      }
+    }
+  }
 }
